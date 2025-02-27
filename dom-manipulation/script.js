307cbcc7eb34e6fb815c14333af747775bc88490
Sadiya Gambo <prettysmiler6@gmail.com>
2025-02-27 08:00:21 -0500

    changes again

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -21,6 +21,30 @@ function showRandomQuote() {
     quoteDisplay.innerHTML = `<div class="quote-item">"${quote.text}" - <em>${quote.category}</em></div>`;
 }
 
+// Function to create the form for adding a new quote
+function createAddQuoteForm() {
+    const formContainer = document.createElement('div');
+    
+    const quoteInput = document.createElement('input');
+    quoteInput.id = 'newQuoteText';
+    quoteInput.type = 'text';
+    quoteInput.placeholder = 'Enter a new quote';
+    
+    const categoryInput = document.createElement('input');
+    categoryInput.id = 'newQuoteCategory';
+    categoryInput.type = 'text';
+    categoryInput.placeholder = 'Enter quote category';
+    
+    const addButton = document.createElement('button');
+    addButton.textContent = 'Add Quote';
+    addButton.onclick = addQuote;
+
+    formContainer.appendChild(quoteInput);
+    formContainer.appendChild(categoryInput);
+    formContainer.appendChild(addButton);
+
+    document.body.appendChild(formContainer);
+}
 // Function to add a new quote
 function addQuote() {
     const newQuoteText = document.getElementById('newQuoteText').value;
